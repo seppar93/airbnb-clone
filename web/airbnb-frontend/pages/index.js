f85f1974356f96b11b90fdb0334b9898af25c6e2
Sepehr Parirokh <sepehrparirokh@gmail.com>
2022-08-01 10:40:13 -0400

    fetching data

--- web/airbnb-frontend/pages/index.js
+++ web/airbnb-frontend/pages/index.js
@@ -1,8 +1,25 @@
+import { sanityClient } from "../sanity";
 
-
-export default function Home() {
-  return (
-    <>
-    </>
-  )
+export default function Home({properties}) {
+  console.log(properties);
+  return <>test</>;
 }
+
+export const getServerSideprops = async () => {
+  const query = '*[ _type == "property"]';
+  const properties = await sanityClient.fetch(query);
+
+  if (!property.length) {
+    return {
+      props: {
+        properties: [],
+      },
+    };
+  } else {
+    return {
+      props:{
+        properties
+      }
+    }
+  }
+};
